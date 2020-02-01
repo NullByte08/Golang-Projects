5fd56cc80f73b771a0d0b403761a0104c187c257
NullByte08 <cheenusoni10@gmail.com>
2020-02-01 23:45:40 +0530

    Third Commit

--- Structs/main.go
+++ Structs/main.go
@@ -2,21 +2,48 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
 
-	//alex := person{firstName: "Alex", lastName: "Anderson"}
-
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			"alex@gmail.com",
+			92341,
+		},
+	}
 	//OR
+	/*var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	alex.contactInfo = contactInfo{"alex@gmail.com", 92341}*/
 
-	var alex person
-	alex.firstName="Alex"
-	alex.lastName="Anderson"
+	alex.updateFirstName("Alexander") // alex will be passed by value to updateFirstName function,
+	// i.e. contents of alex will be copied and THEN used by updateFirstName function.
+	pointerAlex := &alex
+	pointerAlex.updateFirstNameUsingPointer("Alexander") // By using pointers we can work on actual values, rather than the copies.
+	alex.printPerson()                                   // Actual firstName of alex will not be updated
+}
 
+func (p person) printPerson() {
+	fmt.Printf("%+v", p) // %+v prints the field names also. %v prints only the values.
+}
 
-	fmt.Println(alex)
+func (p person) updateFirstName(firstName string) {
+	p.firstName = firstName
+}
+
+func (p *person) updateFirstNameUsingPointer(firstName string) {
+	(*p).firstName = firstName
 }
